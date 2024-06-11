3e9864d8102198d3283d1269c205f6445f0a45aa
Barış <barislegomaster@gmail.com>
2024-06-11 10:26:12 +0300

    init

---
+++ TaleworldsWorkshopProject/Program.cs
@@ -0,0 +1,85 @@
+﻿public class Game
+{
+    private Game() { }
+
+    public static Game Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Game();
+            }
+            return _instance;
+        }
+    }
+
+    private static Game? _instance;
+
+
+    public void StartGame()
+    {
+        Console.WriteLine("Started new game");
+    }
+
+    public void Help()
+    {
+        Console.WriteLine("Help page");
+    }
+
+    public void ExitGame()
+    {
+        Console.WriteLine("Exited game");
+        _instance = null;
+    }
+
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+    }
+}
+
+
+public class Player
+{
+
+}
+
+
+public class Enemy
+{
+
+}
+
+
+public class Boss
+{
+
+}
+
+
+public class Inventory
+{
+
+}
+
+
+public class Item
+{
+
+}
+
+public class Items
+{
+
+}
+
+public class Weapon : Item
+{
+
+}
+
+public class Shop
+{
+
+}
