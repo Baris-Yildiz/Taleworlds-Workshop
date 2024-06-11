97b2f8d1314a86a208fc2e88e5f6ce6a5c4bc5fb
Barış <barislegomaster@gmail.com>
2024-06-11 11:26:22 +0300

    player classı içine bazı fieldlar eklendi ve constructor yazıldı.

--- TaleworldsWorkshopProject/Player.cs
+++ TaleworldsWorkshopProject/Player.cs
@@ -3,8 +3,10 @@ public class Player
 {
     private int _health;
     private int _attackPower;
-    private Inventory _inventory;
+    private Inventory _inventory = null;
+    private Weapon _currentWeapon = null;
 
+    //Oyuncumuza Player.Instance olarak erişebiliyoruz ve sadece 1 tane oyuncumuz olabiliyor.
     public static Player Instance
     {
         get
@@ -18,7 +20,16 @@ public class Player
         }
     }
 
-    private static Player _instance;
+    private static Player? _instance;
 
-    private Player() { }
+    // player ilk değerlerini initialize eder.
+    private Player() {
+        _health = 100;
+        _attackPower = 10;
+    }
+
+    private void _destroyPlayer()
+    {
+        _instance = null;
+    }
 }
